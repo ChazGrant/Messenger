e4be58abeef8f09dca16a163e47e2ca2a0f3aaac
ChazGrant <ChazGrant@users.noreply.github.com>
2020-11-12 18:59:14 +0300

    Bug fixes

--- imp.py
+++
@@ -1,6 +0,0 @@
-from incaps import *
-
-tmp = Test(1,2,3)
-print(tmp.one)
-print(tmp._two)
-print(tmp._Test__three)
--- incaps.py
+++
@@ -1,7 +0,0 @@
-class Test:
-    def __init__(self, one, two, three):
-        self.one = one
-        self._two = two
-        self.__three = three
-
-
